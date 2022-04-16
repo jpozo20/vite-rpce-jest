f009f8fac273bba0e6a8167ca1e4ed6d5b6cfee2
Jefry Pozo <jefrypozo@gmail.com>
2022-04-16 19:34:56 -0400

    Initial template commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "app/app";
+
+import "./index.css";
+
+
+ReactDOM.render(
+	<React.StrictMode>
+	    <div className="absolute h-full w-full flex items-center justify-center bg-gray-200">
+            <span className="text-lg">Welcome to Vite + React.js</span>
+        </div>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import manifest from "./public/manifest.json";
+import { crx } from "rollup-plugin-chrome-extension";
+
+
+export default defineConfig((configEnv) => {
+	const isDevelopment = configEnv.mode === "development";
+	const jsxRuntime = isDevelopment ? "classic" : "automatic";
+
+	return {
+		plugins: [
+			tsconfigPaths(),
+			react({
+				jsxRuntime: jsxRuntime,
+			}),            
+			crx({
+				manifest,
+			}),
+		],
+		resolve: {
+			alias: {
+				src: resolve(__dirname, "src"),
+				app: resolve(__dirname, "src", "app"),
+				assets: resolve(__dirname, "src", "assets"),
+				components: resolve(__dirname, "src", "components"),
+				hooks: resolve(__dirname, "src", "hooks"),
+			},
+		},
+		css: {
+			modules: {
+				generateScopedName: isDevelopment ? "[name]__[local]__[hash:base64:5]" : "[hash:base64:5]",
+			},
+		},
+	};
+});
