1ed2618563c5b0bce79c0fb3d6dbafbd34e759f7
Jefry Pozo <jefrypozo@gmail.com>
2022-04-16 20:16:34 -0400

    Fix error with invalid import

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "app/app";
-
-import "./index.css";
-
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
 ReactDOM.render(
-	<React.StrictMode>
-	    <div className="absolute h-full w-full flex items-center justify-center bg-gray-200">
-            <span className="text-lg">Welcome to Vite + React.js</span>
-        </div>
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+   <React.StrictMode>
+      <div className="absolute h-full w-full flex items-center justify-center bg-gray-200">
+         <span className="text-lg">Welcome to Vite + React.js</span>
+      </div>
+   </React.StrictMode>,
+   document.getElementById("root")
+)
